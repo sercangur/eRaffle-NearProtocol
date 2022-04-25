950c6fea1599c8aba295b3a00ed3bfbb5291c691
Kazım Sercan GÜR <ksercangur@gmail.com>
2022-04-25 23:29:18 +0300

    Update 6.report.sh

--- scripts/6.report.sh
+++ scripts/6.report.sh
@@ -3,23 +3,21 @@ set -e
 
 [ -z "$NEAR_ENV" ] && echo "Missing \$NEAR_ENV environment variable" && exit 1
 [ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable" && exit 1
-[ -z "$OWNER" ] && echo "Missing \$OWNER environment variable" && exit 1
 
 echo "These are the environment variables being used:"
 echo
 echo "CONTRACT is [ $CONTRACT ]"
-echo "OWNER is [ $OWNER ]"
 echo
 echo
 
 echo "--------------------------------------------"
 echo Report for $CONTRACT
 echo "--------------------------------------------"
-echo "near call \$CONTRACT getAllTickets '{}' --accountId \$OWNER"
-near call $CONTRACT getAllTickets '{}' --accountId $OWNER
+echo "near view \$CONTRACT getAllTickets '{}'"
+near view $CONTRACT getAllTickets '{}'
 echo
 
-echo "near call \$CONTRACT getAllRaffles '{}' --accountId \$OWNER"
-near call $CONTRACT getAllRaffles '{}' --accountId $OWNER
+echo "near view \$CONTRACT getAllRaffles '{}'"
+near view $CONTRACT getAllRaffles '{}'
 echo
 echo
